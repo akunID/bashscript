4db83063ea3f648383363ebe8c98b6b64a55d056
thommykennedy <thommykennedy@gmail.com>
2021-02-10 23:49:34 +0700

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.1.0/node_exporter-1.1.0.linux-amd64.tar.gz
 tar -zxvf node_exporter-1.1.0.linux-amd64.tar.gz
-mv node_exporter-1.1.0.linux-amd64.tar.gz/node_exporter /usr/local/bin/
+mv node_exporter-1.1.0.linux-amd64/node_exporter /usr/local/bin/
 
 cat <<EOF > /lib/systemd/system/prometheus-node_exporter.service {
 [Unit]
